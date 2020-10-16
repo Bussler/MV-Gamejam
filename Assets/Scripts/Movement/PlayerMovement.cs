d61fd32a8b04b529c5f075583c08a140be0f2cc3
Bussler <maarten.bussler@gmail.com>
2020-10-16 16:49:19 +0200

    movement test

--- Assets/Scripts/Movement/PlayerMovement.cs
+++ Assets/Scripts/Movement/PlayerMovement.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float moveSpeed = 6f;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    //movement
+    private void FixedUpdate()
     {
-        
+        float moveSide = Input.GetAxis("Horizontal");
+        float moveUp = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveSide, moveUp);
+        float amtToMove = moveSpeed * Time.deltaTime;
+
+        transform.Translate(movement*amtToMove, Space.World);
+
     }
 }
