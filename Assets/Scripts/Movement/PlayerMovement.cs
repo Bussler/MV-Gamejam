e05b16bb520da62ce5227599317fdec3072a9e05
Bussler <maarten.bussler@gmail.com>
2020-10-16 16:17:56 +0200

    yeet

--- Assets/Scripts/Movement/PlayerMovement.cs
+++ Assets/Scripts/Movement/PlayerMovement.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ´test : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
