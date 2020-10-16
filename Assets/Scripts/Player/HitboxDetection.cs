7ca72690ef89c6b273ff2c20e18975dfcbc6709b
Luca <hohmann.luca@tutanota.com>
2020-10-16 20:13:09 +0200

    Added hitbox detection and new tags for enemy projectiles

---
+++ Assets/Scripts/Player/HitboxDetection.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+// Script which is responsible for all hitbox detections from a player with something
+public class HitboxDetection : MonoBehaviour
+{
+    private BoxCollider2D _playerHitbox;
+
+    private String _playerTag = "Player";
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag.Equals("EnemyProjectile"))
+        {
+            Debug.Log("HIT " + other.gameObject.GetComponent<EnemyProjectile>().damage);
+            int damage = other.gameObject.GetComponent<EnemyProjectile>().damage;
+            StatManager.StatManagerInstance.DecreaseLifePoints(damage);
+            return;
+        }
+        
+        // flower tags are built like this: Flower-1 / Flower-2 .....
+        String[] objectTags = other.gameObject.tag.Split('-');
+        if (objectTags[0].Equals("Flower"))
+        {
+            
+            int flowerType = int.Parse(objectTags[1]);
+            StatManager.StatManagerInstance.SetCurrentPollType(flowerType);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _playerHitbox = gameObject.GetComponent<BoxCollider2D>();
+    }
+}
