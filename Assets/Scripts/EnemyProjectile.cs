35fef6ced5b941baf9f879cc4dd43241796034ed
JoshuaWeggartner <34662787+JoshuaWeggartner@users.noreply.github.com>
2020-10-16 17:36:55 +0200

    Enemy script angefangen

---
+++ Assets/Scripts/EnemyProjectile.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyProjectile : MonoBehaviour
+{
+    public float speed;
+    public float lifetime;
+    public int damage;
+    public Vector3 moveVec;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Translate(moveVec * speed * Time.deltaTime,Space.World);
+        lifetime -= Time.deltaTime;
+        if (lifetime <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+    public void OnTriggerEnter(Collider other)
+    {
+        //Test for player and do damage
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Gegner.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gegner : MonoBehaviour
+{
+
+    public int health;
+    public float moveSpeed;
+    public enum MovementType
+    {
+        Random,
+        FollowPlayer,
+        Stationary,
+        BounceOffWall
+    }
+    public MovementType moveType;
+    public float timeBetweenDirectionChange;
+    private Vector3 randomDirection;
+    public float attackPerSecond;
+    public enum AttackType
+    {
+        ShootTowardsPlayer,
+        ShootRandomDirection,
+        ShootSpecificDiretions
+    }
+    public AttackType attackType;
+    public Vector2[] directions;
+    public GameObject projectile;
+
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(moveType== MovementType.Random)
+        {
+            InvokeRepeating("ChangeRandomDirection",0f,timeBetweenDirectionChange);
+        }
+        InvokeRepeating("Attack", 1/ attackPerSecond ,1/ attackPerSecond);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        switch (moveType)
+        {
+            case MovementType.Random:
+                this.transform.Translate(randomDirection * moveSpeed * Time.deltaTime);
+                break;
+
+                case MovementType.FollowPlayer:
+                this.transform.Translate((player.transform.position - this.transform.position).normalized * moveSpeed*Time.deltaTime);
+                break;
+
+            case MovementType.Stationary:
+
+                break;
+
+            case MovementType.BounceOffWall:
+
+                break;
+        }
+
+
+
+
+    }
+
+    public void TakeDamage(int amount)
+    {
+        health -= amount;
+        if(health <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Destroy(this.gameObject);
+    }
+
+    public void ChangeRandomDirection()
+    {
+        randomDirection = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0).normalized;
+    }
+
+    public void Attack() {
+
+        switch (attackType)
+        {
+            case AttackType.ShootTowardsPlayer:
+               GameObject g= Instantiate(projectile, this.transform.position, Quaternion.identity);
+                g.transform.right = player.transform.position - g.transform.position;
+                g.GetComponent<EnemyProjectile>().moveVec = (player.transform.position - g.transform.position).normalized;
+
+
+                break;
+
+            case AttackType.ShootRandomDirection:
+                GameObject k = Instantiate(projectile, this.transform.position, Quaternion.identity);
+                Vector3 m = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0).normalized;
+                k.transform.right = m;
+                k.GetComponent<EnemyProjectile>().moveVec = m;
+                break;
+
+            case AttackType.ShootSpecificDiretions:
+
+                for(int i=0; i < directions.Length; i++)
+                {
+                    GameObject p = Instantiate(projectile, this.transform.position, Quaternion.identity);
+                    p.transform.right = directions[i];
+                    p.GetComponent<EnemyProjectile>().moveVec = directions[i].normalized;
+                }
+                break;
+
+           
+        }
+
+
+    }
+}
