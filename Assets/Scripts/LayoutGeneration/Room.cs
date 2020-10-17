03bd5e5b12886103be6ee6822eb2c5ab6b63b7da
Philipp Eckart-Weißbäcker <philipp@mewww.de>
2020-10-17 18:31:54 +0200

    FloorLayout Generator added

---
+++ Assets/Scripts/LayoutGeneration/Room.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room 
+{   
+
+    public enum RoomType
+    {
+        Starting,
+        Boss,
+        Shop,
+        Item,
+        Regular,
+        Empty
+    }
+
+    public RoomType roomType;
+
+    //Roomcoordinates on floor
+
+    public int coordX;
+
+    public int coordY;
+
+    public bool doorWayNorth = false;
+
+    public bool doorWaySouth = false;
+
+    public bool doorWayWest = false;
+
+    public bool doorWayEast = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    bool allDoors()
+    {
+        return doorWayNorth && doorWayEast && doorWaySouth && doorWayWest;
+    }
+}
