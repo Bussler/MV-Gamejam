b9139097358c5b0359efbbd0a87343c5cd50e2e6
Bussler <maarten.bussler@gmail.com>
2020-10-17 19:43:27 +0200

    quick ui fix
    TODO maybe update nectar only every time we change it and not every frame

--- Assets/Scripts/Menu/GUISetup.cs
+++ Assets/Scripts/Menu/GUISetup.cs
@@ -24,6 +24,7 @@ public class GUISetup : MonoBehaviour
     {
         showHearts();
         showSprints();
+        Nektar.text = "Nectar: " + StatManager.StatManagerInstance.GetNectarAmount();
     }
 
     public void showHearts()
