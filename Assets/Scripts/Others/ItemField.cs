e7391ba9aa7789330ebe6495b5f5cb1ee4c0cc75
Bussler <maarten.bussler@gmail.com>
2020-10-17 23:04:04 +0200

    fast push for project structure and sprite activation

--- Assets/Scripts/Others/ItemField.cs
+++ Assets/Scripts/Others/ItemField.cs
@@ -7,11 +7,13 @@ public class ItemField : MonoBehaviour
     public Item itemToBuy;
     public int indexForBuy;
 
-    public void showPrice()//TODO
+    public void showPrice()
     {
         GameObject text = transform.GetChild(0).gameObject;
         text.GetComponent<TextMesh>().text = "Cost: " + itemToBuy.cost;
         text.SetActive(true);
+
+        this.GetComponent<SpriteRenderer>().sprite = itemToBuy.image;
     }
 
     private void OnTriggerEnter2D(Collider2D collision) //buy the item
