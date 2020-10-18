e62a0910aa1573ec1c0752a53803d872e4a58220
Bussler <maarten.bussler@gmail.com>
2020-10-18 10:32:31 +0200

    item blink if not able to buy

--- Assets/Scripts/Others/ItemField.cs
+++ Assets/Scripts/Others/ItemField.cs
@@ -6,6 +6,7 @@ public class ItemField : MonoBehaviour
 {
     public Item itemToBuy;
     public int indexForBuy;
+    public bool blinking = false;
 
     public void showPrice()
     {
@@ -23,7 +24,45 @@ public class ItemField : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision) //buy the item
     {
         if (Shop.ShopInstance.buyItem(indexForBuy))
+        {
             Destroy(gameObject);//destroy gameobject after buy
+        }
+        else
+        {
+            if (!blinking)
+            {
+                Debug.Log("Hahaeee");
+                StartCoroutine(DoBlinks(2, 0.2f));
+                blinking = true;
+            }
+        }
+    }
+
+
+    IEnumerator DoBlinks(int blinks, float blinkTime)
+    {
+        bool half = false;
+        Color cur = GetComponent<SpriteRenderer>().color;
+        Debug.Log("Haha");
+        for (int i = 0; i < blinks * 2; i++)
+        {
+            if (half)
+            {
+                this.GetComponent<SpriteRenderer>().color = new Color(cur.r, cur.g, cur.b, 1);
+                half = false;
+            }
+            else
+            {
+                this.GetComponent<SpriteRenderer>().color = new Color(cur.r, cur.g, cur.b, 0.3f);
+                half = true;
+            }
+            //wait for a bit
+            yield return new WaitForSeconds(blinkTime);
+        }
+
+        //make sure renderer is setup when we exit
+        this.GetComponent<SpriteRenderer>().color = new Color(cur.r, cur.g, cur.b, 1);
+        blinking = false;
     }
 
 }
